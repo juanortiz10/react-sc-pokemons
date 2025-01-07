a38482cfe15701d58d47998fa782b0eadb14b10e
Juan Ortiz Jr <juanjo.jjo94@gmail.com>
2025-01-06 18:32:54 -0600

    Add first commit

---
+++ src/app/utils/index.ts
@@ -0,0 +1,9 @@
+export const getPokemons = async (limit: number, offset: number) => {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`);
+
+    if (!response.ok) {
+        throw new Error("Failed to fetch poke API");
+    }
+
+    return response.json();
+};
